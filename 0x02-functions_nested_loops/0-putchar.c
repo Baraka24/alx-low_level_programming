0bf53615459c94f1db4982be8c13696e898fa2bf
Baraka Kinywa <bkinywa24@gmail.com>
2023-02-21 14:24:57 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include 'main.h'
+#include "main.h"
 /**
- * main - prints _putchar, followed by a new line.
- * return 0
- */
+ * main - to print '-putchar as a string
+ * Return: return 0
+*/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }

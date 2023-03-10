b2cbf787bd4bbbe49ce2b6c683eb2d8a88f023b4
Baraka Kinywa <bkinywa24@gmail.com>
2023-03-10 11:45:49 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,16 @@
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * main - print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argv; /*Ignore argv*/                                                                                                         
-printf("%i\n", argc - 1);                                                                                                            
-                                                                                                                                     
-return (0);                                                                                                                          
+#include <stdio.h>
+#include "main.h"
+/**
+* main - print number of arguments passed to program
+* @argc: argument counter
+* @argv: pointer to array of arguments
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+if (argc > 0)
+printf("%d\n", argc - 1);
+
+return (0);
 }

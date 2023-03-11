3f96abfb5197142472198d254ef527c6770255c5
Baraka Kinywa <bkinywa24@gmail.com>
2023-03-11 09:28:12 -0800

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,8 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-/*a list that was given from the task 0 */
+#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
-int _isalpha(int c);
+int _isaplpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
@@ -23,5 +20,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
